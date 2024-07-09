82cd15db65c313233c432eb376631c1d3491f03c
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-07-09 08:59:51 -0300

    chore: fixed basepath

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,13 @@ import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 
-const basePath = import.meta.env.VITE_BASE_PATH || ''
-
 function MainRouter() {
   return (
     <Router>
       <Routes>
-        <Route path={`${basePath}`} element={<Home/>} />
-        <Route path={`${basePath}/home`} element={<Home/>} />
-        <Route path={`${basePath}/login`} element={<Login/>} />
+        <Route path={``} element={<Home/>} />
+        <Route path={`/home`} element={<Home/>} />
+        <Route path={`/login`} element={<Login/>} />
       </Routes>
     </Router>
   );
