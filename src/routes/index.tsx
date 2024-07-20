358d89644debe9a8b5c1eab68ffa43a2aa2c83ca
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-07-19 21:48:43 -0300

    chore: changed react router and baseurl on vite

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,15 @@
-
-import { HashRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from '../pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
 
 function MainRouter() {
   return (
     <Router>
       <Routes>
-        <Route path={`/`} element={<Home/>} />
-        <Route path={`/home`} element={<Home/>} />
+        <Route path={`/`} element={<Home />} />
+        <Route path={`/home`} element={<Home />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default MainRouter;
+export default MainRouter
