5b77fa6eb7c5b8793d62a9006a0ea87b6ff1f925
Cristian Ralf <cristian.rlf@gmail.com>
2024-08-08 19:39:19 -0300

    Mudanças viuais do figma

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,110 +1,31 @@
 import './Home.css'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+  const navigate = useNavigate()
+
   return (
     <>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum
-      molestie dignissim. In hac habitasse platea dictumst. Sed tincidunt
-      ultricies dui, quis suscipit nunc. Nam eleifend leo quis magna pretium
-      scelerisque. Curabitur eget arcu dapibus, luctus lectus a, faucibus nisi.
-      Donec at tellus condimentum, dictum dolor ac, elementum quam. Suspendisse
-      aliquet eros sed diam porta, nec feugiat neque elementum. Phasellus
-      tristique enim nec maximus pulvinar. Aliquam erat volutpat. Etiam faucibus
-      nibh ut felis mollis, ac eleifend mi facilisis. Nam dignissim felis enim,
-      non finibus orci pharetra et. Curabitur fermentum rutrum neque, ut
-      interdum nibh fringilla et. Praesent fringilla, risus fermentum tincidunt
-      ultricies, mauris leo rhoncus mi, vel feugiat erat erat vel mi. Morbi sit
-      amet posuere libero. Integer scelerisque sodales nibh ac eleifend. Sed
-      lorem massa, dictum eget tortor sit amet, ultrices bibendum mauris. Mauris
-      sed efficitur diam. Donec mauris arcu, faucibus eu blandit nec, faucibus a
-      sem. Integer fringilla eget enim vitae laoreet. Curabitur dignissim libero
-      ex, a porttitor turpis posuere eget. Curabitur urna nunc, tempus a
-      fermentum id, vehicula eget tellus. Phasellus velit tellus, malesuada non
-      urna ut, bibendum hendrerit nibh. Pellentesque porta sagittis sapien non
-      volutpat. Nulla a magna ac turpis consequat tempor. Duis porta condimentum
-      tempor. Praesent accumsan et ex sed aliquam. Aliquam ultricies aliquet ex,
-      id sodales felis tempus eu. Cras dapibus ipsum convallis libero dignissim
-      molestie. Sed a sapien eu urna venenatis ullamcorper nec at enim. Mauris
-      ornare dolor enim, non tincidunt nisl lobortis sit amet. Donec laoreet
-      auctor diam nec interdum. Vestibulum ante ipsum primis in faucibus orci
-      luctus et ultrices posuere cubilia curae; Nulla ac diam consequat nulla
-      ullamcorper iaculis id bibendum libero. Vivamus vitae ipsum id lacus
-      facilisis sagittis in non sapien. Sed consectetur finibus tellus sit amet
-      blandit. Quisque id massa nec turpis egestas varius. Donec eget mattis
-      tortor, vitae rutrum nunc. Pellentesque habitant morbi tristique senectus
-      et netus et malesuada fames ac turpis egestas. Duis vel nibh diam.
-      Praesent a fermentum lectus, tincidunt feugiat sapien. Vestibulum turpis
-      lectus, consequat non nisi vel, efficitur dignissim urna. Proin mollis
-      rutrum est, eget commodo nibh congue eu. Morbi pellentesque mauris arcu,
-      vel faucibus arcu facilisis vel. Nulla dapibus pharetra lectus non
-      rhoncus. Duis gravida libero sit amet pharetra commodo. Etiam sit amet
-      interdum dolor, non dapibus purus. Nullam nibh tellus, tempor at urna nec,
-      venenatis tempus nisi. In id blandit tellus. Vivamus semper nulla nunc, ac
-      sollicitudin nisl accumsan ac. Phasellus posuere ac massa eu consectetur.
-      Aenean sollicitudin, libero nec tincidunt pharetra, nulla odio hendrerit
-      ligula, non hendrerit nunc risus vitae ligula. Phasellus pharetra commodo
-      dolor. Praesent tortor tellus, pellentesque vitae sapien ut, semper
-      hendrerit diam. Nullam vitae arcu non neque blandit aliquet. Morbi non
-      risus et justo varius dapibus eu vel lectus. Fusce ut felis sed lacus
-      fringilla posuere. Phasellus vitae massa id ex posuere imperdiet.
-      Suspendisse sit amet metus egestas, mattis lacus mattis, tempor eros.
-      Mauris pellentesque tortor eget aliquet tempus. Proin blandit risus
-      libero, a egestas lectus cursus at. Fusce nulla sapien, luctus lobortis
-      neque ac, imperdiet condimentum eros. Sed et pulvinar orci, non ultrices
-      ligula. Suspendisse scelerisque consequat dapibus. Cras eu hendrerit
-      turpis. Proin convallis viverra turpis sit amet euismod. Proin varius
-      pellentesque mi, at aliquam metus consectetur a. Praesent erat quam,
-      bibendum eu augue eget, lacinia dictum metus. Etiam vitae libero purus.
-      Nunc sodales accumsan diam et sollicitudin. Praesent vitae lacus semper,
-      porttitor purus sit amet, volutpat velit. Donec libero libero, ultricies
-      ac quam sit amet, aliquam convallis ipsum. Vivamus vitae sem sodales
-      sapien consequat facilisis sit amet eu nulla. Duis condimentum pretium
-      fermentum. Praesent viverra ligula ultrices varius viverra. Cras in magna
-      eu neque porttitor tristique. Phasellus est sem, tristique sed consectetur
-      ut, imperdiet at diam. Nulla interdum sem eu blandit placerat. Class
-      aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-      himenaeos. Nam molestie nisi nec massa lacinia pellentesque. Mauris
-      congue, turpis quis sollicitudin imperdiet, urna ipsum porttitor urna, non
-      rutrum purus orci a felis. Donec tempus nunc vitae faucibus fringilla.
-      Duis tincidunt suscipit tincidunt. Nam rutrum mi gravida varius tristique.
-      Nullam accumsan, arcu at imperdiet cursus, lacus leo blandit nulla, eu
-      blandit ex arcu quis arcu. Maecenas ac lorem magna. Aenean vehicula arcu
-      ut urna bibendum ullamcorper. Vivamus eget neque cursus, bibendum risus
-      et, venenatis est. Etiam a sodales mauris. Praesent egestas arcu sed augue
-      fringilla maximus. Nulla a quam ipsum. Praesent ultricies, mauris vitae
-      mollis fringilla, dui magna faucibus quam, et ornare mi urna ac justo.
-      Duis vel leo in arcu commodo pharetra. Vestibulum eu sagittis est. Donec
-      vel velit et nisl luctus commodo non at ex. Praesent sit amet sem quam.
-      Curabitur porttitor sit amet arcu et porta. Curabitur tempor, justo et
-      suscipit dictum, quam ligula vulputate nunc, sagittis egestas sapien
-      turpis quis nibh. Pellentesque mollis, justo a pretium dictum, ex est
-      sagittis odio, quis molestie lectus nulla et diam. Sed vestibulum ac neque
-      eget dictum. Mauris varius at quam volutpat cursus. Orci varius natoque
-      penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc
-      rhoncus elementum semper. Vestibulum pellentesque aliquet massa nec
-      condimentum. Donec in venenatis est, sed euismod nulla. Nulla convallis
-      lorem id nunc condimentum, in finibus erat congue. Donec pulvinar ante in
-      quam bibendum, vehicula tristique nibh dictum. Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit. Sed ornare arcu quis nisl egestas cursus.
-      Praesent felis augue, facilisis a arcu ut, porttitor lobortis justo.
-      Integer dapibus scelerisque nibh ut tempus. Phasellus luctus commodo
-      libero, eget tincidunt urna volutpat eu. Integer varius iaculis dui sed
-      sollicitudin. Etiam libero ipsum, gravida ut aliquet in, faucibus sed
-      diam. Vivamus eget nisi eu nisi laoreet sagittis non id mi. Aliquam sed
-      quam ac sem placerat lacinia ut nec magna. Quisque vitae luctus urna.
-      Donec enim enim, luctus in nunc in, pharetra condimentum ante. Curabitur
-      lobortis nibh mauris, eu facilisis libero hendrerit eget. Suspendisse non
-      nunc ultricies, lobortis est vitae, tempus libero. Aliquam erat volutpat.
-      Quisque nunc orci, interdum vitae sem nec, tempor ultricies quam.
-      Curabitur eu dui magna. Suspendisse potenti. Proin et commodo lectus.
-      Vivamus malesuada purus non ex euismod aliquet. Aliquam molestie risus
-      vitae ante hendrerit vestibulum. Proin porta convallis mi vel euismod. Ut
-      interdum placerat est, quis tristique mi finibus in. Vestibulum consequat
-      risus id hendrerit tempus. Donec eu auctor arcu. Ut dignissim, diam quis
-      egestas auctor, mi quam elementum lacus, ut consequat purus est sed nulla.
-      Nullam pharetra ligula at eleifend consequat. Quisque id ex sed diam
-      auctor eleifend. Nulla cursus ipsum ac felis interdum eleifend. Maecenas
-      id quam orci. Quisque lacinia ultricies semper.
+      <section className="hero-container">
+        <div className="hero-section" id="hero-content">
+          <div className="hero-text">
+            Criamos o seu
+            <strong> sistema, site ou aplicativo</strong> com as melhores
+            tecnologias disponíveis
+          </div>
+          <div className="hero-buttons">
+            <button
+              className="btn btn-primary"
+              onClick={() => navigate('/contact')}
+            >
+              Fale conosco
+            </button>
+          </div>
+        </div>
+        <div className="hero-section" id="hero-image">
+          <img src="../../public/assets/hero-image.png" alt="Hero Image" />
+        </div>
+      </section>
     </>
   )
 }
