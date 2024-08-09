93e8aaf7fa53612bb9e3228be22f666df38d510c
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-08-08 21:46:04 -0300

    style: Added more styling on about section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,93 +15,102 @@ function Home() {
   return (
     <>
       <section className="hero-container" id="hero-container">
-        <div className="hero-section" id="hero-content">
-          <div className="hero-text">
-            Criamos o seu
-            <strong> sistema, site ou aplicativo</strong> com as melhores
-            tecnologias disponíveis
+        <section className="container">
+          <div className="d-flex">
+            <div className="hero-section" id="hero-content">
+              <div className="hero-text">
+                Criamos o seu
+                <strong> sistema, site ou aplicativo</strong> com as melhores
+                tecnologias disponíveis
+              </div>
+              <div className="hero-buttons">
+                <a href="#contact" className="btn btn-primary">
+                  Fale Conosco <FaArrowRight />
+                </a>
+              </div>
+            </div>
+            <div className="hero-section" id="hero-image">
+              <img src="/assets/hero-image.png" alt="Hero Image" />
+            </div>
           </div>
-          <div className="hero-buttons">
-            <a href="#contact" className="btn btn-primary">
-              Fale Conosco <FaArrowRight />
-            </a>
-          </div>
-        </div>
-        <div className="hero-section" id="hero-image">
-          <img src="/assets/hero-image.png" alt="Hero Image" />
-        </div>
+        </section>
       </section>
       <section id="about">
-        <h1>Quem Somos</h1>
-        <section>
-          <p>
-            A CodeForge Builders é uma empresa baiana de software fundada por um
-            programador apaixonado por desafios e com vários anos de experiência
-            no desenvolvimento de soluções tecnológicas. Nascemos com o desejo
-            de transformar ideias em realidade, unindo criatividade e expertise
-            técnica para entregar o melhor em cada projeto.
-          </p>
-          <p>
-            Especializados em desenvolvimento de software, nossa equipe é
-            dedicada a criar experiências excepcionais em três áreas principais:
-            aplicações web, desenvolvimento mobile e soluções personalizadas.
-          </p>
-          <div className="d-flex gap-3 mb-3">
-            <Card>
-              <CardBody>
-                <CardTitle>Desenvolvimento Web</CardTitle>
-                <CardText>
-                  Seja para criar um blog envolvente, uma plataforma robusta ou
-                  um site corporativo, nossa equipe é especialista em
-                  desenvolver aplicações web completas. Trabalhamos tanto no
-                  front-end quanto no back-end, garantindo que cada projeto não
-                  apenas funcione perfeitamente, mas também ofereça uma
-                  experiência de usuário intuitiva e atraente. Nossos serviços
-                  incluem o desenvolvimento de sistemas personalizados,
-                  integração com APIs e a implementação das melhores práticas em
-                  design responsivo.
-                </CardText>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardBody>
-                <CardTitle>Desenvolvimento Mobile</CardTitle>
-                <CardText>
-                  Em um mundo cada vez mais móvel, entendemos a importância de
-                  estar presente em todas as plataformas. Desenvolvemos
-                  aplicativos móveis para iOS e Android, focados em desempenho e
-                  usabilidade. Se você precisa de um aplicativo para engajar
-                  seus usuários em movimento ou para otimizar processos
-                  internos, temos a expertise necessária para entregar soluções
-                  eficazes e inovadoras.
-                </CardText>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardBody>
-                <CardTitle>Soluções Personalizadas</CardTitle>
-                <CardText>
-                  Além de criar sites atraentes e funcionais, também oferecemos
-                  soluções personalizadas que atendem às necessidades
-                  específicas do seu negócio. Seja para blogs dinâmicos,
-                  plataformas interativas ou sites corporativos, garantimos que
-                  cada solução seja alinhada com seus objetivos e proporcione
-                  uma presença online marcante.
-                </CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <p>
-            Nosso time é extremamente enxuto e eficiente, buscando entregar as
-            soluções no menor tempo possível dentro do escopo de conhecimento do
-            time.
-          </p>
-          <p>
-            Na Codeforge, nosso compromisso é com a excelência e a satisfação do
-            cliente. Trabalhamos em estreita colaboração com você para entender
-            suas necessidades e entregar produtos que superem suas expectativas.
-            Vamos juntos transformar sua visão digital em realidade!
-          </p>
+        <section className="container">
+          <h1>Quem Somos</h1>
+          <section>
+            <p>
+              A CodeForge Builders é uma empresa baiana de software fundada por
+              um programador apaixonado por desafios e com vários anos de
+              experiência no desenvolvimento de soluções tecnológicas. Nascemos
+              com o desejo de transformar ideias em realidade, unindo
+              criatividade e expertise técnica para entregar o melhor em cada
+              projeto.
+            </p>
+            <p>
+              Especializados em desenvolvimento de software, nossa equipe é
+              dedicada a criar experiências excepcionais em três áreas
+              principais: aplicações web, desenvolvimento mobile e soluções
+              personalizadas.
+            </p>
+            <div className="d-flex gap-3 mb-3">
+              <Card>
+                <CardBody>
+                  <CardTitle>Desenvolvimento Web</CardTitle>
+                  <CardText>
+                    Seja para criar um blog envolvente, uma plataforma robusta
+                    ou um site corporativo, nossa equipe é especialista em
+                    desenvolver aplicações web completas. Trabalhamos tanto no
+                    front-end quanto no back-end, garantindo que cada projeto
+                    não apenas funcione perfeitamente, mas também ofereça uma
+                    experiência de usuário intuitiva e atraente. Nossos serviços
+                    incluem o desenvolvimento de sistemas personalizados,
+                    integração com APIs e a implementação das melhores práticas
+                    em design responsivo.
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody>
+                  <CardTitle>Desenvolvimento Mobile</CardTitle>
+                  <CardText>
+                    Em um mundo cada vez mais móvel, entendemos a importância de
+                    estar presente em todas as plataformas. Desenvolvemos
+                    aplicativos móveis para iOS e Android, focados em desempenho
+                    e usabilidade. Se você precisa de um aplicativo para engajar
+                    seus usuários em movimento ou para otimizar processos
+                    internos, temos a expertise necessária para entregar
+                    soluções eficazes e inovadoras.
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody>
+                  <CardTitle>Soluções Personalizadas</CardTitle>
+                  <CardText>
+                    Além de criar sites atraentes e funcionais, também
+                    oferecemos soluções personalizadas que atendem às
+                    necessidades específicas do seu negócio. Seja para blogs
+                    dinâmicos, plataformas interativas ou sites corporativos,
+                    garantimos que cada solução seja alinhada com seus objetivos
+                    e proporcione uma presença online marcante.
+                  </CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <p>
+              Nosso time é extremamente enxuto e eficiente, buscando entregar as
+              soluções no menor tempo possível dentro do escopo de conhecimento
+              do time.
+            </p>
+            <p>
+              Na Codeforge, nosso compromisso é com a excelência e a satisfação
+              do cliente. Trabalhamos em estreita colaboração com você para
+              entender suas necessidades e entregar produtos que superem suas
+              expectativas. Vamos juntos transformar sua visão digital em
+              realidade!
+            </p>
+          </section>
         </section>
       </section>
       <section id="team"></section>
