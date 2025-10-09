779307a7876e75e411aff75c8c26d53f2a63c7b0
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2025-10-08 22:34:34 -0300

    feat: added content to Hero section
    - Added Title to Hero Section
    - Added Paragraphs to Hero Section
    - Fixed translations

--- frontend/src/components/Hero/index.tsx
+++ frontend/src/components/Hero/index.tsx
@@ -1,7 +1,24 @@
+import { useTranslations } from 'next-intl';
 import styles from './Hero.module.css'
+import PrimaryLinkButton from '../PrimaryLinkButton';
 
 export default function Hero() {
+  const t = useTranslations();
+
+  const paragraphCount = 3;
+  const paragraphs = Array.from({ length: paragraphCount }).map((_, i) => t(`Hero.Content.${i}`));
+
   return <section className={`${styles.heroSection} w-screen h-screen flex justify-center`}>
-    <div className="flex justify-start items-center w-11/12"></div>
+    <div className="flex justify-start items-center w-11/12">
+      <div className='flex flex-wrap w-9/15 gap-6'>
+        <h2 className='text-5xl font-semibold w-6/11'>{t('Hero.Title')}</h2>
+        {
+          paragraphs.map((text, i) => (
+            <p className='text-xl' key={i}>{text}</p>
+          ))
+        }
+        <PrimaryLinkButton href="#" className='flex gap-2'>🚀{t('Hero.CTAButtonLabel')}</PrimaryLinkButton>
+      </div>
+    </div>
   </section>
 }
