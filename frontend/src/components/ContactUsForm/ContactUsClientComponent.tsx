f82c88e2e3ba70c388e6c341a60751282b48517a
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2025-11-18 20:18:14 -0300

    feat: started new phone input implementation

--- frontend/src/components/ContactUsForm/ContactUsClientComponent.tsx
+++ frontend/src/components/ContactUsForm/ContactUsClientComponent.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useTranslations } from "use-intl";
 import Input from "../Input";
-import PhoneInput from "../PhoneInput";
 import Select, { ISelectOption } from "../Select";
 import Textarea from "../Textarea";
 import { CountryData } from "./fetchCountryData";
@@ -28,6 +27,11 @@ export function ContactUsFormClientComponent({ countryData }: ContactUsFormClien
     }, 1500)
   }
 
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  function handlePhoneChange(event: any) {
+
+  }
+
   return <section id="contact-us" className="bg-zinc-900 w-screen flex justify-center">
     <section className="flex flex-col w-11/12 md:w-6/12 gap-4 pb-8">
       <div id="our-services-header" className="flex flex-col p-8 mt-[63px] gap-4">
@@ -37,7 +41,7 @@ export function ContactUsFormClientComponent({ countryData }: ContactUsFormClien
         <Input required id="name-input" name="name-input" label={t('ContactUs.fields.nameField.label')} placeholder={t('ContactUs.fields.nameField.placeholder')} />
         <Input required id="email-input" name="email-input" type="email" label={t('ContactUs.fields.emailField.label')} placeholder={t('ContactUs.fields.emailField.placeholder')} />
         <div className="flex flex-col md:flex-row gap-2">
-          <PhoneInput required name="phone-input" className="grow" id="phone-input" label={t('ContactUs.fields.phoneWhatsappField.label')} countries={countryData} />
+          <Input required id="phone-input" className="grow shrink min-w-0" name="phone-input" type="tel" label={t('ContactUs.fields.phoneWhatsappField.label')} onChange={handlePhoneChange} />
           <Select required name="service-input" className="grow shrink min-w-0" id="service-input" label={t('ContactUs.fields.serviceField.label')} emptyOptionLabel={t('ContactUs.fields.serviceField.placeholder')} options={SelectOptions} />
         </div>
         <Textarea required name="project-description-input" label={t('ContactUs.fields.projectDescriptionField.label')} placeholder={t('ContactUs.fields.projectDescriptionField.placeholder')} rows={4} />
