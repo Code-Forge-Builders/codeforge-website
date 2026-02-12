905442e4b1a6e847808906022168884515340ca1
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2026-02-12 17:30:37 -0300

    feat: Finished email job worker on application

--- backend/internal/jobs/email_job_service.go
+++ backend/internal/jobs/email_job_service.go
@@ -1,8 +1,185 @@
 package jobs
 
+import (
+	"codeforge/website-prospecting-api/internal/db"
+	"codeforge/website-prospecting-api/internal/email"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
 type EmailJobService interface {
 	Start() error
 	Stop() error
 
 	Enqueue(job *BackgroundJob) error
+}
+
+type EmailJobPayload struct {
+	To           []string
+	Subject      string
+	TemplateName string
+	Data         any
+}
+
+type emailJobService struct {
+	emailService email.EmailService
+	stopChan     chan struct{}
+}
+
+func (s *emailJobService) Start() error {
+	s.stopChan = make(chan struct{})
+	go s.run()
+	return nil
+}
+
+func (s *emailJobService) Stop() error {
+	select {
+	case <-s.stopChan:
+		// Job is already stopped
+	default:
+		close(s.stopChan)
+	}
+
+	return nil
+}
+
+func (s *emailJobService) Enqueue(job *BackgroundJob) error {
+	job.Status = BackgroundJobStatusPending
+	job.Attempts = 0
+	job.MaxAttempts = 3
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+
+	result := db.DB.Create(job)
+
+	if result.Error != nil {
+		return fmt.Errorf("failed to enqueue email job: %v", result.Error)
+	}
+
+	return nil
+}
+
+func (s *emailJobService) claimNextJob(tx *gorm.DB) (*BackgroundJob, error) {
+	now := time.Now()
+	job := BackgroundJob{}
+
+	result := tx.
+		Clauses(clause.Locking{
+			Strength: "UPDATE",
+			Options:  "SKIP LOCKED",
+		}).
+		Where("status = ?", BackgroundJobStatusPending).
+		Where("type = ?", BackgroundJobTypeEmail).
+		Where("locked_at IS NULL OR locked_at < ?", now.Add(-1*time.Minute)).
+		Where("attempts < max_attempts").
+		Order("created_at ASC").
+		Order("updated_at ASC").
+		First(&job)
+
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("failed to get next email job: %v", result.Error)
+	}
+
+	job.Status = BackgroundJobStatusInProgress
+	job.LockedAt = &now
+	job.UpdatedAt = now
+
+	result = tx.Save(&job)
+	if result.Error != nil {
+		return nil, fmt.Errorf("failed to claim email job: %v", result.Error)
+	}
+
+	return &job, nil
+}
+
+func (s *emailJobService) processJob(job *BackgroundJob) error {
+	now := time.Now()
+	payload := EmailJobPayload{}
+	err := json.Unmarshal(job.Payload, &payload)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal email job payload: %v", err)
+	}
+
+	newEmail := email.Email{
+		To:      payload.To,
+		Subject: payload.Subject,
+	}
+
+	newEmail.HTML, err = email.RenderTemplate(payload.TemplateName, payload.Data)
+	if err != nil {
+		return fmt.Errorf("failed to render email template: %v", err)
+	}
+
+	err = s.emailService.SendEmail(newEmail)
+
+	job.UpdatedAt = now // Update the updated_at timestamp to the current time
+
+	if err != nil {
+		job.Attempts++
+		job.LockedAt = nil
+
+		if job.Attempts >= job.MaxAttempts {
+			job.Status = BackgroundJobStatusFailed // Dead letter
+		} else {
+			job.Status = BackgroundJobStatusPending // Retry
+		}
+
+		result := db.DB.Save(job)
+		if result.Error != nil {
+			return fmt.Errorf("failed to update email job: %v", result.Error)
+		}
+		return fmt.Errorf("failed to send email: %v", err)
+	}
+
+	job.Status = BackgroundJobStatusSuccess
+	job.FinishedAt = &now // Update the finished_at timestamp to the current time
+	job.LockedAt = nil
+
+	result := db.DB.Save(job)
+	if result.Error != nil {
+		return fmt.Errorf("failed to update email job: %v", result.Error)
+	}
+
+	return nil
+}
+
+func (s *emailJobService) run() {
+	for {
+		select {
+		case <-s.stopChan:
+			return
+		default:
+			var job *BackgroundJob
+			err := db.DB.Transaction(func(tx *gorm.DB) error {
+				var err error
+				job, err = s.claimNextJob(tx)
+				return err
+			})
+
+			if err != nil {
+				time.Sleep(1 * time.Second) // Wait a bit to avoid db hammering
+				continue
+			}
+
+			if job == nil { // No job found, wait a bit to avoid db hammering
+				time.Sleep(1 * time.Second)
+				continue
+			}
+
+			err = s.processJob(job)
+			if err != nil {
+				log.Printf("failed to process email job: %v\n", err)
+				continue
+			}
+		}
+	}
 }
